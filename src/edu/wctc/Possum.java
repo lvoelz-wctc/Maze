12319716712a7c43f9be400600cc543230255488
lvoelz_wctc <luke.voelz@gmail.com>
2021-02-01 10:03:17 -0800

    Added inventory clear after interacting in possum room.

--- src/edu/wctc/Possum.java
+++ src/edu/wctc/Possum.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 /**Player fights the direpossum in this room**/
 
 public class Possum extends Room implements Interactable{
-    public String interact = "You show the Direpossum your inventory. He seems uninterested in the steak and carrot,\n but" +
-            "drinks the cup of metafiction. He realizes that he's a character in a program. This validates his\n" +
+    public String interact = "You give the Direpossum your inventory. He seems uninterested in the steak and carrot,\n but" +
+            " drinks the cup of metafiction. He realizes that he's a character in a program. This validates his\n" +
             "feelings of existential anxiety, which he thanks you for.";
     public String description = "The room appears to be a dungeon. A Direpossum sits in the center. Interact with the room" +
             " to use your inventory on it.";
@@ -20,6 +20,7 @@ public class Possum extends Room implements Interactable{
     @Override
     public String interact(Player player) {
         player.addToScore(1);
+        player.inventory.clear();
         return interact;
     }
 
